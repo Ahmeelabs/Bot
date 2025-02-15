3e712b5a30254028fcd48bb61ce05c5363f36d9d
Ahmeelab <fbreported1@gmail.com>
2025-02-15 23:54:30 +0500

    Update 0_PREM-ANTI-NAME.js

--- scripts/events/0_PREM-ANTI-NAME.js
+++ scripts/events/0_PREM-ANTI-NAME.js
@@ -1,20 +1,28 @@
-module.exports.config = {
-  name: "antiname",
-  eventType: ["log:user-nickname"],
-  version: "0.0.1",
-  credits: "PREM BABU",
-  description: "THIS BOT WAS MADE BY JULMI JAAT"
+lmodule.exports.config = {
+	name: "autosetname",
+	eventType: ["log:subscribe"],
+	version: "1.0.3",
+	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	description: "Automatically set new member nicknames"
 };
 
-module.exports.run = async function({ api, event, Users, Threads }) {
-    var { logMessageData, threadID, author } = event;
-    var botID = api.getCurrentUserID();
-    var { BOTNAME, ADMINBOT } = global.config;
-    var { nickname } = await Threads.getData(threadID, botID);
-    var nickname = nickname ? nickname : BOTNAME;
-    if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
-        api.changeNickname(nickname, threadID, botID)
-        var info = await Users.getData(author);
-       return api.sendMessage({ body: `सॉरी बॉस आप मेरा नाम चेंज नही कर सकते 🙂✌️`}, threadID);
-    }  
-        }
+module.exports.run = async function({ api, event, Users }) {
+const { threadID } = event;
+var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId)
+	for (let idUser of memJoin) {
+		const { readFileSync, writeFileSync } = global.nodemodule["fs-extra"];
+		const { join } = global.nodemodule["path"]
+		const pathData = join("./modules/commands","cache", "autosetname.json");
+		var dataJson = JSON.parse(readFileSync(pathData, "utf-8"));
+		var thisThread = dataJson.find(item => item.threadID == threadID) || { threadID, nameUser: [] };
+		if (thisThread.nameUser.length == 0) return 
+		if (thisThread.nameUser.length != 0) {  
+		var setName = thisThread.nameUser[0] 
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		var namee1 = await api.getUserInfo(idUser)
+        var namee = namee1[idUser].name
+		api.changeNickname(`${setName} ${namee}`, threadID, idUser);
+		} 
+	}	
+	return api.sendMessage(`Set a temporary nickname for the new member`, threadID, event.messageID)
+}
