86716deab4a70b49e8a845baa53ad1c3ddce3108
Ahmeelab <fbreported1@gmail.com>
2025-02-15 23:58:41 +0500

    Update 0owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -1,64 +1,27 @@
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-
-module.exports = {
-config: {
-  name: "owner",
-  aurthor:"Tokodori",// Convert By Goatbot Tokodori 
-   role: 0,
-  shortDescription: " ",
-  longDescription: "",
-  category: "admin",
-  guide: "{pn}"
-},
-
-  onStart: async function ({ api, event }) {
-  try {
-    const ownerInfo = {
-      name: '🔰𝙊𝙒𝙉𝙀𝙍 𝙄𝙉𝙁𝙊🔰\n\n ◉❖जुल्मी≛जाट❖◉\ (─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─)(•◡•)\n\n𝐀𝐠𝐞 : 23\n\n𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 𝐖𝐢𝐭𝐡 :-🄿🅁🄴🄴🅃(ɴᴀᴅᴀɴ ʙᴄʜɪ)\n\n𝐅𝐫𝐨𝐦 : 𝐉𝐈𝐍𝐃(𝐇𝐀𝐑𝐘𝐀𝐍𝐀)\n\n𝐒𝐭𝐮𝐝𝐲 : 𝐂𝐨𝐦𝐩𝐮𝐭𝐞𝐫 𝐏𝐫𝐨𝐠𝐫𝐚𝐦𝐦𝐢𝐧𝐠\n\n𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :-👇👇https://www.facebook.com/julmijaat1008?mibextid=ZbWKwL \n\n𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦 𝐋𝐢𝐧𝐤:-👉 :https://instagram.com/_julmi_jaat__?igshid=OGQ5ZDc2ODk2ZA==',
-      gender: 'Male',
-      age: '24',
-      height: '5.10',
-      facebookLink: 'https://www.facebook.com/julmijaat1008?mibextid=ZbWKwL',
-      nick: 'JULMI', 
-    };
-
-    const bold = 'https://i.imgur.com/HnxHi6i.jpeg'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
-
-    const tmpFolderPath = path.join(__dirname, 'tmp');
-
-    if (!fs.existsSync(tmpFolderPath)) {
-      fs.mkdirSync(tmpFolderPath);
-    }
-
-    const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
-    const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
-
-    fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
-
-    const response = `
-Owner Information:🧾
-Name: ${ownerInfo.name}
-Gender: ${ownerInfo.gender}
-Age: ${ownerInfo.age}
-Height: ${ownerInfo.height}
-Facebook: ${ownerInfo.facebookLink}
-Nick: ${ownerInfo.nick}
-`;
-
-
-    await api.sendMessage({
-      body: response,
-      attachment: fs.createReadStream(videoPath)
-    }, event.threadID, event.messageID);
-
-    if (event.body.toLowerCase().includes('ownerinfo')) {
-      api.setMessageReaction('🚀', event.messageID, (err) => {}, true);
-    }
-  } catch (error) {
-    console.error('Error in ownerinfo command:', error);
-    return api.sendMessage('An error occurred while processing the command.', event.threadID);
-  }
-},
+const fs = require("fs");
+module.exports.config = {
+  name: "admin2",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩", 
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "admin",
+    cooldowns: 5, 
 };
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("ADMIN")==0 || event.body.indexOf("Admin")==0 || event.body.indexOf("/Admin")==0 || event.body.indexOf("#admin")==0) {
+    var msg = {
+        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅",
+        attachment: 
+fs.createReadStream(__dirname + `/noprefix/profile.png`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
